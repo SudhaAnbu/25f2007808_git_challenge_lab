c1997b586fd2bffce2affda5b50a345f34cd6209
25f2007808 <25f2007808@ds.study.iitm.ac.in>
2026-01-22 08:57:25 +0530

    Added main python file

---
+++ main.py
@@ -0,0 +1,9 @@
+from sum  import add
+from difference import substract
+from product import multiply
+
+
+print("Sum:", add(10,5))
+print("Difference:", subtract(10,5))
+print("Product:", multiply(10,5))
+
